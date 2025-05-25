e7a0037c64a5f4c49ada7a1bcfb7eac42d3c2491
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-05-25 17:13:30 +0700

    changed styling for contacts to accomodate mobile

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -18,9 +18,11 @@ export default function Contacts({
     transition={{ duration: 0.5 }}
   >
       <span className="sub-heading">Contact and Socials</span>
+      <div className="socials">
         <h1 className="contacts-subheading"><FaLinkedin /> <a href="https://linkedin.com/in/jlojanarungsiri" target="_blank">jlojanarungsiri</a></h1>
-        <h1 className="contacts-subheading"><IoMailOpenOutline /> <a href="mailto:lojanarungsiri.j@northeastern.edu" target="_blank">lojanarungsiri.j@northeastern.edu</a></h1>
+        <h1 className="contacts-subheading"><IoMailOpenOutline /> <a href="mailto:jlojanarungsiri@gmail.com" target="_blank">jlojanarungsiri@gmail.com</a></h1>
         <h1 className="contacts-subheading"><FaGithub /> <a href="https://github.com/Bartavius" target="_blank">Bartavius</a></h1>
+      </div>
       <div className="smaller-section-divider"></div>
       <div className="modal-footer">
         <button
