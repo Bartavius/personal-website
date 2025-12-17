176c5e5ac9e3d2f7997ddc52dde5ae0d65a2cca4
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-12-17 08:26:42 +0700

    recalculated gpa

--- src/sections/EducationSection/EducationSection.tsx
+++ src/sections/EducationSection/EducationSection.tsx
@@ -30,7 +30,7 @@ export default function EducationSection() {
               </span>
               <span>
                 <b>Awards:</b> Dean's List Fall 2023, Spring 2024, Fall 2024,
-                Spring 2025
+                Spring 2025, Fall 2025
               </span>
             </span>
           </div>
